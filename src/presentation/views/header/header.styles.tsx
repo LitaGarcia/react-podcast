a83e7b74b802434acdcec803a3d3b5e633012713
Manuela Garcia <lita.garcia@kairosds.com>
2023-10-09 16:09:57 +0200

    FEAT: Create styles to header with styles components

---
+++ src/presentation/views/header/header.styles.tsx
@@ -0,0 +1,11 @@
+
+import styled from "styled-components";
+
+export const Nav = styled.nav `
+  margin: 1.5rem 1.5rem 0.5rem 1.5rem ;
+  cursor: pointer;
+`
+export const Link = styled.a`
+  color: cornflowerblue;
+  font-size: 1.5rem;
+`;
--- src/presentation/views/header/header.tsx
+++ src/presentation/views/header/header.tsx
@@ -1,7 +1,15 @@
+import {useNavigate} from "react-router-dom";
+import {Link, Nav} from "./header.styles"
+
 function Header() {
+    const navigate = useNavigate();
     return (
         <>
-            podcaster
+            <Nav>
+            <Link onClick={() => navigate('/')}>
+                Podcaster
+            </Link>
+            </Nav>
         </>
     )
 }
